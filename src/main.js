e955da4c4d8aa36cc7326610ed3336f5f94dc1ea
GalvinGao <galvingaoper@gmail.com>
2020-01-26 00:02:09 -0500

    initialized i18n

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+
+  outputDir: '_',
+  runtimeCompiler: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'zh',
+      fallbackLocale: 'zh',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
 }
