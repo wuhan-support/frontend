7279212892c3f05aa772ea978bd32964f51f1274
GalvinGao <galvingaoper@gmail.com>
2020-01-26 02:13:41 -0500

    fix(main): removed suivue

--- src/main.js
+++ src/main.js
@@ -5,12 +5,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
-import SuiVue from 'semantic-ui-vue';
-
 import * as Sentry from '@sentry/browser';
 import * as Integrations from "@sentry/integrations";
-
-Vue.use(SuiVue);
 
 Vue.config.productionTip = false
 
@@ -20,12 +16,6 @@ if (production) {
   Sentry.init({
     dsn: 'https://9636aaa824a744f98a619df0aaabba00@sentry.io/1536764',
     integrations: [new Integrations.Vue({Vue, attachProps: true})],
-
-    // NOTE: the config below (`logErrors`) controls whether the error will be logged
-    // to the console or not. Considering we are in production, logging
-    // errors to the console is not appropriate (since we are using Sentry).
-    // So I've turned this setting off. If necessary please re-enable it.
-    // More info at: https://docs.sentry.io/platforms/javascript/vue/
     logErrors: false,
     release: 'frontend-v2@' + (Vue.config.version || 'unknown'),
   });
