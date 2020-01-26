9b8ce1922727d45274df55540b256b9862e6c429
GalvinGao <galvingaoper@gmail.com>
2020-01-26 01:07:23 -0500

    fix(main): re-enabled sentry logerrors

--- src/main.js
+++ src/main.js
@@ -17,13 +17,7 @@ if (production) {
   Sentry.init({
     dsn: 'https://237b425bb92749e98198df60f1835d31@sentry.io/1973056',
     integrations: [new Integrations.Vue({Vue, attachProps: true})],
-
-    // NOTE: the config below (`logErrors`) controls whether the error will be logged
-    // to the console or not. Considering we are in production, logging
-    // errors to the console is not appropriate (since we are using Sentry).
-    // So I've turned this setting off. If necessary please re-enable it.
-    // More info at: https://docs.sentry.io/platforms/javascript/vue/
-    logErrors: false,
+    logErrors: true,
     release: 'frontend@' + (config.version || 'unknown'),
   });
 }
