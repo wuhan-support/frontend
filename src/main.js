54fcd3271cbffd23c69a9e9373631a649561e000
GalvinGao <galvingaoper@gmail.com>
2020-01-26 01:04:55 -0500

    style(eslint): fix eslint

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
-
-Vue.config.productionTip = false
-
 import * as Sentry from '@sentry/browser';
 import * as Integrations from "@sentry/integrations";
+import  config from "./config";
+
+Vue.config.productionTip = false
 
 const production = process.env.NODE_ENV === 'production';
 
