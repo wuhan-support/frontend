c25a63146fbab9d0a02167be0ea6227f9272e370
GalvinGao <galvingaoper@gmail.com>
2020-01-26 01:11:49 -0500

    style(eslint): fix eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@
           class="my-3"
           contain
           height="200"
-        ></v-img>
+        />
       </v-flex>
 
       <v-flex mb-4>
@@ -20,7 +20,10 @@
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
         </p>
       </v-flex>
 
@@ -28,7 +31,9 @@
         mb-5
         xs12
       >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
 
         <v-layout justify-center>
           <a
@@ -47,7 +52,9 @@
         xs12
         mb-5
       >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
 
         <v-layout justify-center>
           <a
@@ -66,7 +73,9 @@
         xs12
         mb-5
       >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
 
         <v-layout justify-center>
           <a
