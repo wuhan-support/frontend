5a36216c9c9e61dd557f19b9531b0bb9fcf2d294
PC_Aaron <1021048274@qq.com>
2020-02-02 13:58:05 +0800

    test:commit

--- src/views/roles/Sponsor.vue
+++ src/views/roles/Sponsor.vue
@@ -87,7 +87,8 @@ export default {
   },
   computed: {
     routes () {
-      return this.$router.options.routes.find(el => el.name === this.$route.matched[0].name).children.filter(el => el.path !== "" && !el.meta.hide)
+      return this.$router.options.routes.find(el =>
+        el.name === this.$route.matched[0].name).children.filter(el => el.path !== "" && !el.meta.hide)
     }
   },
 }
