fd6b9a29d864dc5cfc1888cae473a0ff8fe41554
Hikaru <i@rua.moe>
2020-02-05 14:35:49 +0800

    Interface optimization

--- src/views/roles/Volunteer.vue
+++ src/views/roles/Volunteer.vue
@@ -9,7 +9,7 @@
         <v-img
           :src="require('@/assets/logo/red.svg')"
           aspect-ratio="2"
-          max-width="192px"
+          max-width="256px"
           contain
           class="mx-auto"
         />
