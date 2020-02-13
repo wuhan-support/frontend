7862fd2ed2debd7c0b157fdcb303045425728018
GalvinGao <galvingaoper@gmail.com>
2020-02-13 01:30:26 -0500

    取消字体内嵌加载优化

---
+++ vue.config.js
@@ -0,0 +1,52 @@
+const Critters = require('critters-webpack-plugin');
+
+module.exports = {
+  'transpileDependencies': [
+    'vuetify'
+  ],
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost/",
+        pathRewrite: {'^/api' : ''}
+      }
+    }
+  },
+
+  outputDir: 'dist',
+  runtimeCompiler: true,
+  integrity: false,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'zh',
+      fallbackLocale: 'zh',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new Critters({
+        preload: "body",
+        fonts: false
+      })
+    ]
+  }
+
+  // pwa: {
+  //   name: "feiyan.help",
+  //   themeColor: "#813d34",
+  //   msTileColor: "#1d499b",
+  //   appleMobileWebAppStatusBarStyle: "black-translucent",
+  //
+  //   workboxPluginMode: "GenerateSW",
+  //   // iconPaths: {
+  //   //   favicon32: 'favicon/favicon-32x32.png',
+  //   //   favicon16: 'favicon/favicon-16x16.png',
+  //   //   appleTouchIcon: 'favicon/apple-touch-icon.png',
+  //   //   maskIcon: 'favicon/safari-pinned-tab.svg',
+  //   //   msTileImage: 'favicon/mstile-150x150.png'
+  //   // }
+  // },
+}
